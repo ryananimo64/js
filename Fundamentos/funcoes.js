2520c4c52933174ad342902aa82b3ff09ed3c8b6
Ryan Rodrigues <ryananimo64@gmail.com>
2025-01-22 16:40:40 -0300

    Aula 2 - JS

--- Fundamentos/funcoes.js
+++ Fundamentos/funcoes.js
@@ -8,3 +8,51 @@ function hello(){
 }
 
 hello()
+
+console.log(typeof(hello))
+hello()
+
+//Função atribuída
+const hello2 = function() {
+    console.log("Hello function assigned")
+}
+
+console.log(typeof(hello2))
+hello2()
+
+//arrow function (simplificação da função atribuída)
+//function ---- =>
+const hello3 = () => {
+    console.log("Hello arrow function")
+}
+
+console.log(typeof(hello3))
+hello3()
+
+//Funções com passagem de parâmetros e retorno
+// função literal
+function somarS(num1, num2) {
+    return (console.log(num1+num2))
+}
+
+somarS(9, 1)
+
+// Função atribuída
+const somarA = function(num1, num2){
+    return (console.log(num1 + num2))
+}
+
+somarA(6, 3)
+
+// Arrow Function
+const somarAF = (num1, num2) => {
+    return (console.log(num1 + num2))
+}
+
+somarAF(60, 9)
+
+//Arrow Function simplificada (retorno é implicito)
+const somarSAS = (num1,num2) => console.log(num1+num2)
+
+
+somarSAS(1, 5)
