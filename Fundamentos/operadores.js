b66c2b5d20e649170c3c80812b8555f8efbee8b8
Ryan Rodrigues <ryananimo64@gmail.com>
2025-01-22 17:19:25 -0300

    Aula 2 - JS

---
+++ Fundamentos/operadores.js
@@ -0,0 +1,34 @@
+/**
+ * Operadores
+ * -arime
+ *  
+ * */
+
+console.clear()
+console.log("Operadores Ariméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | modulo(Resto da divisão)")
+console.log("")
+console.log("Operadores Ariméticos de atribuíção")
+console.log("+= | soma e atribuíção a variavel")
+console.log("-= | subtração e atribuíção a variavel")
+console.log("*= | multiplicação e atribuíção a variavel")
+console.log("/= | divisão e atribuíção a variavel")
+console.log("++ | somar 1 ao resultado")
+console.log("-- | subtrair 1 ao resultado")
+console.log("")
+console.log("Comparadores")
+console.log(">  | maior que")
+console.log("<  | menor que")
+console.log(">= | maior ou igual a")
+console.log("<= | menor ou igual a")
+console.log("===| idêntico a")
+console.log("!==| não idêntico a")
+console.log("")
+console.log("Operadores lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("!  | NOT")
