e29b75d978915df54babb3424ed6ed98121c000b
ryananimo64 <ryananimo64@gmail.com>
2025-02-05 14:13:50 -0300

    .

---
+++ vetor/array.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @author Ryan Rodrigues
+ */
+
+console.clear()
+let alunos = []
+console.log(typeof(alunos))
+//                 [0]       [1]      [2]     [3]
+let alunosem1 = ["Vitor","Claudio","Pedro","Mario"]
+console.log(typeof(alunos))
+// .length (obtem o tamanho do array)
+console.log(alunosem1.length)
+// Exibindo os dados de um array
+console.log(alunosem1)
+// Exibindo um aluno no array
+console.log(alunosem1[2])
+// Exibindo os dados do array em uma tabela
+console.table(alunosem1)
